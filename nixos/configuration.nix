9b7955bb08b540d782b7b51cf587e2447d11a304
Sakshat <30901276+sakshatshinde@users.noreply.github.com>
2024-06-22 11:44:36 +0530

    Update configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -205,17 +205,6 @@
     };
   };
 
-  # Perform garbage collection weekly to maintain low disk usage
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 1w";
-  };
-
-  # Refer to the following link for more details:
-  # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-auto-optimise-store
-  nix.settings.auto-optimise-store = true;
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "24.05";
 }
